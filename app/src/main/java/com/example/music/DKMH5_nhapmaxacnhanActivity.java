0c21b660493f523c14c7a9fa85046ee9a710577e
26CheMauHanh <125116943+26CheMauHanh@users.noreply.github.com>
2024-12-24 15:14:43 +0700

    Update and rename dkmh5_nhapmaxacnhanActivity.java to DKMH5_nhapmaxacnhanActivity.java

--- app/src/main/java/com/example/music/DKMH5_nhapmaxacnhanActivity.java
+++ app/src/main/java/com/example/music/DKMH5_nhapmaxacnhanActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class dkmh5_nhapmaxacnhanActivity extends AppCompatActivity {
+public class DKMH5_nhapmaxacnhanActivity extends AppCompatActivity {
 
     private TextView subtitle;
 
